1a602f16a7eeac2fb2dcd1e7543a9ca172ef644a
Jakob Næss <github@nspc.no>
2025-04-07 21:21:15 +0200

    Fixed typo causing major headache

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-HOSTNAME=$(hostname)
-# Fallback in case the hostname is not set or the command fails
-if [ -z "$HOSTNAME" ]; then
-  HOSTNAME="zfsbootmenu"
-fi
-
-/etc/zfsbootmenu/zbm-builder.sh -O --volume=/tmp:/tmp -O --volume=./:/build -O --hostname="$HOSTNAME"
